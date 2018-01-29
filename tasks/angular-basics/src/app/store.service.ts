3c0cb71db918a6a3a09a99b2cb0c3dc047241f1e
arsafab <dmitriibely@gmail.com>
2018-01-29 18:19:44 +0300

    angular-basics/fix: add types to the store service

--- tasks/angular-basics/src/app/store.service.ts
+++ tasks/angular-basics/src/app/store.service.ts
@@ -4,12 +4,12 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class StoreService {
 
-  private items = new BehaviorSubject<any>([]);
-  private item = new BehaviorSubject<any>({});
-  private task = new BehaviorSubject<any>({});
-  private tasks = new BehaviorSubject<any>([]);
-  private state = new BehaviorSubject<any>(false);
-  private target = new BehaviorSubject<any>({});
+  private items = new BehaviorSubject<object[]>([]);
+  private item = new BehaviorSubject<object>({});
+  private task = new BehaviorSubject<object>({});
+  private tasks = new BehaviorSubject<object[]>([]);
+  private state = new BehaviorSubject<boolean>(false);
+  private target = new BehaviorSubject<object>({});
 
   store = this.items.asObservable(); // categories with subitems and tasks
   activeCategory = this.item.asObservable(); // category that is opened at the moment
@@ -18,54 +18,54 @@ export class StoreService {
   isOpen = this.state.asObservable(); // property for the category buttons/ If true - remove, edit and add btn is hided
   targetCategory = this.target.asObservable(); // temp for category in where a task will be moved
 
-  undoState;
-  redoState;
+  undoState: string;
+  redoState: string;
 
   constructor() { }
 
-  updateStore(arr) {
+  updateStore(arr: object[]): void {
     this.items.next(arr);
   }
 
-  updateCategory(obj) {
+  updateCategory(obj: object): void {
     this.item.next(obj);
   }
 
-  updateTask(obj) {
+  updateTask(obj: object): void {
     this.task.next(obj);
   }
 
-  updateAllTasks(arr) {
+  updateAllTasks(arr: object[]): void {
     this.tasks.next(arr);
   }
 
-  updateOpenState(bool) {
+  updateOpenState(bool: boolean): void {
     this.state.next(bool);
   }
 
-  updateTargetCategory(obj) {
+  updateTargetCategory(obj: object): void {
     this.target.next(obj);
   }
 
-  redo() {
+  redo(): void {
     if (this.redoState) {
       this.updateUndo();
       this.items.next(JSON.parse(this.redoState));
     }
   }
 
-  updateRedo() {
+  updateRedo(): void {
     this.redoState = JSON.stringify(this.items.getValue());
   }
 
-  undo() {
+  undo(): void {
     if (this.undoState) {
       this.updateRedo();
       this.items.next(JSON.parse(this.undoState));
     }
   }
 
-  updateUndo() {
+  updateUndo(): void {
     this.undoState = JSON.stringify(this.items.getValue());
   }
 }
