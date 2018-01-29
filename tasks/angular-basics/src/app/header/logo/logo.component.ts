da7f6e6d97346bfeec47cac0a0c3d1488a71e98d
arsafab <dmitriibely@gmail.com>
2018-01-29 18:22:12 +0300

    angular-basics/fix: clear header component

--- tasks/angular-basics/src/app/header/logo/logo.component.ts
+++ tasks/angular-basics/src/app/header/logo/logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
@@ -11,11 +11,6 @@ import { Component, OnInit } from '@angular/core';
   `,
   styleUrls: ['./logo.component.scss']
 })
-export class LogoComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class LogoComponent {
 }
